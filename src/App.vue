8acfd28e8a9f35239ae237898268c641fc62c0b9
gigor <i@gigor.ru>
2023-04-05 18:01:46 +0400

    set up vue with tailwind

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+  <div class="w-full h-full flex">
+    <main>
+      Hell o world
+    </main>
+
+    <aside>
+        menu
+    </aside>
+  </div>  
+</template>
